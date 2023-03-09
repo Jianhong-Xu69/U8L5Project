28dc6c624ed1bdf2eee141fca38a217b23db5133
BT_4N2_15 <jxu5406@bths.edu>
2023-03-09 12:13:35 -0500

    Patchnotes v1.01
    Added a runner class for clients to run

---
+++ src/EncryptorRunner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EncryptorRunner {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        System.out.println("How many rows?");
+        int r = Integer.parseInt(user.nextLine());
+        System.out.println("How many columns?");
+        int c = Integer.parseInt(user.nextLine());
+        Encryptor test = new Encryptor(r,c);
+
+        System.out.println("(E)ncrypt or (D)ecrypt?");
+        String a = user.nextLine();
+        System.out.println("What is the message?");
+        String m = user.nextLine();
+        if (a.equalsIgnoreCase("e")){
+            System.out.println(test.encryptMessage(m));
+        } else {
+            System.out.println(test.decryptMessage(m));
+        }
+    }
+}
